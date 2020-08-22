0e7719765822fe0bc1aa80b09e3797102a4deda7
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-08-22 13:44:48 -0300

    added animation to the project

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const game = () =>{
         const introScreen = document.querySelector(".intro");
         const match = document.querySelector(".match");
 
-
         playBtn.addEventListener("click", () => {
             introScreen.classList.add("fadeOut");
             match.classList.add("fadeIn")
@@ -18,17 +17,28 @@ const game = () =>{
         const options = document.querySelectorAll(".options button");
         const playerHand = document.querySelector(".player-hand");
         const computerHand = document.querySelector(".computer-hand");
+        const hands = document.querySelectorAll(".hands img")
 
+        hands.forEach(hand =>{
+            hand.addEventListener("animationend", function(){
+                this.style.animation = "";
+            })
+        })
         const computerOptions = ["rock", "paper", "scissors"];
         options.forEach(option=>{
             option.addEventListener("click", function(){
                 const computerNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[computerNumber];
 
-                compareHands(this.textContent,computerChoice);
+                setTimeout(() =>{
+                    compareHands(this.textContent,computerChoice);
 
-                playerHand.src = `./assets/${this.textContent}.png`;
-                computerHand.src = `./assets/${computerChoice}.png`;
+                    playerHand.src = `./assets/${this.textContent}.png`;
+                    computerHand.src = `./assets/${computerChoice}.png`;
+     
+                }, 2000);
+                playerHand.style.animation = "shakePlayer 2s ease"
+                computerHand.style.animation = "shakeComputer 2s ease"
             });
         });
     };
